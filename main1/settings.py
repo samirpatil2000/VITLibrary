96470532524e321dfdcb1fe8b2344cef5a1d9802
samir patil <samirspatil742099@gmail.com>
2021-05-02 02:17:47 +0530

    frontend changes

--- main1/settings.py
+++ main1/settings.py
@@ -147,6 +147,7 @@ CRISPY_TEMPLATE_PACK='bootstrap4'
 
 EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
+# EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_HOST_USER='nousername7420@gmail.com'
